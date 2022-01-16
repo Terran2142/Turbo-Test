f5254ebc3246aa8ec5404125d51d401dbb9d0431
Terran2142 <terran_tan@yahoo.com>
2022-01-16 22:58:58 +0800

    Static Test

--- 2022_01_16_Static/main.c
+++ 2022_01_16_Static/main.c
@@ -1,9 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
+extern s;
+
+int test()
+{
+	static int a = 0;
+	a++;
+	return a;
+}
 int main()
 {
-	int a = 15;
+	int i = 0;
+	while (i < 10)
+	{
+		int x = test();
+		x = x + s;
+		printf("%d\n", x);
+		i++;
+	}
 
 	return 0;
 }
