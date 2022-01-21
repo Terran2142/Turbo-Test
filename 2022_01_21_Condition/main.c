f7e8f535bda0922a33cff6c47df3ed5c1c9e1872
Terran2142 <terran_tan@yahoo.com>
2022-01-21 19:52:58 +0800

    条件语句练习

--- 2022_01_21_Condition/main.c
+++ 2022_01_21_Condition/main.c
@@ -2,21 +2,54 @@
 
 #include <stdio.h>
 
+//if else型条件语句
+//int main()
+//{
+//	int num = 0;
+//	int mod = 0;
+//
+//	printf("Input = ");
+//	scanf("%d", &num);
+//
+//	mod = num % 2;
+//
+//	if (mod == 0)
+//		printf("%d 是偶数\n", num);
+//
+//	else
+//		printf("%d 是奇数\n", num);
+//
+//	return 0;
+//}
+
+
+//switch型条件语句
 int main()
 {
-	int num = 0;
-	int mod = 0;
+	int day = 0;
+	int week = 0;
 
-	printf("Input = ");
-	scanf("%d", &num);
+	scanf("%d", &day);
 
-	mod = num % 2;
-
-	if (mod == 0)
-		printf("%d 是偶数\n", num);
-
+	if (day >= 1 && day <= 5)
+		week = 1;
+	else if (day >=5 && day <= 7)
+		week = 2;
 	else
-		printf("%d 是奇数\n", num);
+		week = 3;
+
+	switch (week)
+	{
+	case 1:
+		printf("工作日\n");
+		break;
+	case 2:
+		printf("休息日\n");
+		break;
+	case 3:
+		printf("error!\n");
+		break;
+	}
 
 	return 0;
 }
