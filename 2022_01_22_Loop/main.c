28b1688fe8bf170833eaf9c6296be3e50f2c54ea
Terran2142 <terran_tan@yahoo.com>
2022-01-22 14:54:17 +0800

    循环语句练习

--- 2022_01_22_Loop/main.c
+++ 2022_01_22_Loop/main.c
@@ -2,9 +2,54 @@
 
 #include <stdio.h>
 
+//int main()
+//{
+//	int i = 1;
+//
+//	while (i <= 10)
+//	{
+//		printf("%d\n", i);
+//		i++;
+//	}
+//
+//	return 0;
+//}
+
+
+//EOF - End Of File
+//int main()
+//{
+//	int ch = 0;
+//	//Ctrl + Z - ¶ÁÈ¡½áÊø
+//
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int ch = 0;
+//
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch > '9')
+//			continue;
+//
+//		putchar(ch);
+//	}
+//
+//	return 0;
+//}
+
+
 int main()
 {
-	int a = 0;
+	int i = 0;
 
 	return 0;
 }
